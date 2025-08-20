78fff0f727ed0ca9221e762f967a9433fc38044b
egil10 <egilfure@gmail.com>
2025-08-20 21:18:36 +0200

    Add Oil Fund chart with updated data and configuration

--- src/js/main.js
+++ src/js/main.js
@@ -305,6 +305,7 @@ export async function initializeApp() {
             // Norges Bank data
             loadChartData('interest-rate-chart', 'https://data.norges-bank.no/api/data/IR/M.KPRA.SD.?format=sdmx-json&startPeriod=1945-01-01&endPeriod=2025-08-01&locale=en', 'Key Policy Rate'),
             loadChartData('govt-debt-chart', 'https://data.norges-bank.no/api/data/GOVT_KEYFIGURES/V_O+N_V+V_I+ATRI+V_IRS..B.GBON?endPeriod=2025-08-01&format=sdmx-json&locale=no&startPeriod=1945-01-01', 'Government Debt', 'line'),
+            loadChartData('oil-fund-chart', './data/cached/oil-fund.json', 'Oil Fund'),
             
             // Oslo Stock Exchange data
             loadChartData('oseax-chart', './data/cached/oslo-indices/oseax.json', 'OSEAX - Oslo Stock Exchange All Share Index'),
